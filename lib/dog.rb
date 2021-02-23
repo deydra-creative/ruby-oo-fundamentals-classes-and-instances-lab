de6e1a97bc9c625c3fafd90e42677a05d41566a3
github username <github email address>
2021-02-23 11:42:11 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def bark
+        puts "Woof!"
+      end
+    end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+fido.bark
+snoopy.bark
+lassie.bark
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+    def talk
+        puts "Hello World!
+    end
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
+adele_goldberg.talk 
+alan_kay.talk
